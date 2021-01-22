940845e03e909666e17a54bda5358f5992d6d773
cmalven <chris@malven.co>
2021-01-21 20:52:06 -0600

    Adds basic physics setup.

--- src/modules/ThreeSetup.js
+++ src/modules/ThreeSetup.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import CANNON from 'cannon';
 
 /**
  * Boilerplate module using THREE.js
@@ -20,15 +21,20 @@ class ThreeSetup {
     this.scene;
     this.loader;
     this.controls;
-    this.tacoModel;
-    this.models = [];
+    this.floor;
+    this.itemModel;
+    this.clock;
+    this.itemModels = [];
+    this.threeItems = [];
+    this.worldItems = [];
+    this.debugItems = [];
 
     // Settings
     this.settings = {
       cameraDistance: 5,
       scalePeriod: 500,
       bgColor: 0xeeeeee,
-      modelCount: 10,
+      maxItemCount: 100,
     };
 
     this.init();
@@ -36,14 +42,25 @@ class ThreeSetup {
 
   init = () => {
     this.createGui();
+    this.createWorld();
     this.createApp();
 
     this.loader = new GLTFLoader();
-    this.loader.load('/models/pizza.glb', (gltf) => {
-      this.tacoModel = gltf;
-      for (var idx = 0, length = this.settings.modelCount; idx < length; idx++) {
-        this.models.push(this.tacoModel.scene.clone(true));
-      }
+
+    const itemPromises = [
+      // 'pizzaBox',
+      'pizza',
+      // 'taco',
+    ].map(itemName => {
+      return new Promise(res => {
+        this.loader.load(`/models/${itemName}.glb`, (gltf) => {
+          this.itemModels.push(gltf.scene);
+          res();
+        });
+      });
+    });
+
+    Promise.all(itemPromises).then(() => {
       this.createItems();
       this.update();
     });
@@ -58,14 +75,49 @@ class ThreeSetup {
     window.APP.gui.add(this.settings, 'scalePeriod', 1, 1000);
   }
 
+  createWorld = () => {
+    this.world = new CANNON.World();
+    this.world.gravity.set(0, -9.82, 0);
+    this.world.broadphase = new CANNON.SAPBroadphase(this.world);
+
+    // Physics Materials
+    const defaultMaterial = new CANNON.Material('default');
+
+    // Physics Contact Materials
+    const contactMaterial = new CANNON.ContactMaterial(
+      defaultMaterial,
+      defaultMaterial,
+      {
+        friction: 0.1,
+        restitution: 0.3,
+      }
+    );
+    this.world.addContactMaterial(contactMaterial);
+    this.world.defaultContactMaterial = contactMaterial;
+
+    // Physics Floor
+    const floorShape = new CANNON.Plane();
+    const floorBody = new CANNON.Body();
+    floorBody.mass = 0;
+    floorBody.addShape(floorShape);
+    floorBody.quaternion.setFromAxisAngle(new CANNON.Vec3(-1, 0, 0), Math.PI * 0.5);
+    this.world.addBody(floorBody);
+  }
+
   createApp = () => {
     // Renderer
     this.renderer = new THREE.WebGLRenderer({
-      devicePixelRatio: 1.5,
-      antialias: false,
+      devicePixelRatio: 1,
+      antialias: true,
     });
     this.renderer.setSize(this.appContainer.offsetWidth, this.appContainer.offsetHeight);
     this.appContainer.appendChild(this.renderer.domElement);
+    this.renderer.shadowMap.enabled = true;
+    this.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+
+    // Clock
+    this.clock = new THREE.Clock();
+    this.oldElapsedTime = 0;
 
     // Camera
     this.camera = new THREE.PerspectiveCamera(45, this.appContainer.offsetWidth / this.appContainer.offsetHeight, 1, 10000);
@@ -93,15 +145,43 @@ class ThreeSetup {
     // Directional Light
     let light = new THREE.DirectionalLight(0xffffff, 0.8);
     light.position.set(-1, 1, 1);
+    light.castShadow = true;
+    light.shadow.mapSize.set(1024, 1024);
+    light.shadow.camera.far = 15;
+    light.shadow.camera.left = - 7;
+    light.shadow.camera.top = 7;
+    light.shadow.camera.right = 7;
+    light.shadow.camera.bottom = - 7;
     this.scene.add(light);
-    // const helper = new THREE.DirectionalLightHelper(light);
-    // this.scene.add(helper);
   }
 
   createItems = () => {
+    // Floor
+    this.floor = new THREE.Mesh(
+      new THREE.PlaneBufferGeometry(10, 10),
+      new THREE.MeshStandardMaterial({
+        color: '#ffffff',
+        metalness: 0.01,
+        roughness: 0.8,
+      })
+    );
+    this.floor.receiveShadow = true;
+    this.floor.rotation.x = - Math.PI * 0.5;
+    this.floor.receiveShadow = true;
+    this.scene.add(this.floor);
+
+    // Food items
+    window.setInterval(this.createFood, 200);
+  }
+
+  createFood = () => {
+    if (this.threeItems.length >= this.settings.maxItemCount) return;
+
     const recurseChildren = child => {
       if (child.material) {
         child.material.metalness = 0;
+        child.castShadow = true;
+        child.receiveShadow = true;
       }
 
       if (child.children) {
@@ -109,39 +189,80 @@ class ThreeSetup {
       }
     };
 
-    // Add all models
-    this.models.forEach(model => {
-      recurseChildren(model);
+    // Get a random model and clone it
+    const itemModel = this.itemModels[Math.floor(Math.random() * this.itemModels.length)];
+    const model = itemModel.clone(true);
 
-      this.scene.add(model);
-      const scale = Math.random() * 2 + 0.5;
-      model.scale.set(scale, scale, scale);
-      model.position.set(
-        (Math.random() - 0.5) * 10,
-        (Math.random() - 0.5) * 10,
-        (Math.random() - 0.5) * 10,
-      );
-      model.rotation.set(Math.random() * 20, Math.random() * 20, Math.random() * 20);
-      model.fallSpeed = model.scale.x * 0.05;
+    // Adjust model so that it is centered at 0, 0
+    const modelChild = model.children[0];
+    const childBox = new THREE.Box3().setFromObject(modelChild);
+    const childBoxCenter = childBox.getCenter(new THREE.Vector3());
+    modelChild.position.set(
+      modelChild.position.x - childBoxCenter.x,
+      modelChild.position.y - childBoxCenter.y,
+      modelChild.position.z - childBoxCenter.z
+    );
+
+    // Calculate the bounding box and size of the model
+    const bbox = new THREE.Box3().setFromObject(model);
+    const itemWidth = (bbox.max.x - bbox.min.x);
+    const itemHeight = (bbox.max.y - bbox.min.y);
+    const itemDepth = (bbox.max.z - bbox.min.z);
+
+    // Create the THREE item
+    recurseChildren(model);
+    this.threeItems.push(model);
+    this.scene.add(model);
+
+    // Create the physics item
+    const itemShape = new CANNON.Box(new CANNON.Vec3(itemWidth * 0.5, itemHeight * 0.5, itemDepth * 0.5));
+    const itemBody = new CANNON.Body({
+      mass: 1,
+      position: new CANNON.Vec3(
+        (Math.random() - 0.5) * 5,
+        10,
+        (Math.random() - 0.5) * 5,
+      ),
+      shape: itemShape,
     });
+    this.world.addBody(itemBody);
+    this.worldItems.push(itemBody);
+
+    // Create the debug item
+    const boxGeometry = new THREE.BoxBufferGeometry(itemWidth, itemHeight, itemDepth);
+    const boxMaterial = new THREE.MeshStandardMaterial({
+      metalness: 0.3,
+      roughness: 0.4,
+      transparent: true,
+      opacity: 0.4,
+    });
+    const debugItem = new THREE.Mesh(boxGeometry, boxMaterial);
+    this.debugItems.push(debugItem);
+    this.scene.add(debugItem);
   }
 
   updateItems = () => {
-    this.models.forEach(model => {
-      model.rotation.x += 0.01;
-      model.rotation.y += 0.01;
-      model.rotation.z += 0.01;
+    // Update physics items
+    this.threeItems.forEach((item, idx) => {
+      const worldItem = this.worldItems[idx];
+      item.position.copy(worldItem.position);
+      item.quaternion.copy(worldItem.quaternion);
 
-      const bounds = 10;
-      if (model.position.y > -bounds) {
-        model.position.y -= model.fallSpeed;
-      } else {
-        model.position.y += bounds*2;
-      }
+      // Debug
+      const debugItem = this.debugItems[idx];
+      debugItem.position.copy(worldItem.position);
+      debugItem.quaternion.copy(worldItem.quaternion);
     });
   }
 
   update = () => {
+    const elapsedTime = this.clock.getElapsedTime();
+    const deltaTime = elapsedTime - this.oldElapsedTime;
+    this.oldElapsedTime = elapsedTime;
+
+    // Update physics world
+    this.world.step(1 / 60, deltaTime, 3);
+  
     this.iter++;
     this.updateItems();
     this.renderer.render(this.scene, this.camera);
